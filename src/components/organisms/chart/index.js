07dfae39e10b54fcac4a54dcf6f5682a89c508d9
soffyjay <aminusofiyat@gmail.com>
2022-04-08 22:39:08 +0100

    fixes

--- src/components/organisms/chart/index.js
+++ src/components/organisms/chart/index.js
@@ -15,8 +15,7 @@ const Chart = () => {
 
   //this function helps us derive the date from th data so we can use on the graph.
   const getGraphDates = () => {
-    const newDates =
-      data && Object.keys(data).length > 0 && Object.keys(data.cases);
+    const newDates = data && Object.keys(data.cases);
     setGraphDates(newDates);
   };
 
@@ -28,9 +27,7 @@ const Chart = () => {
 
   useEffect(async () => {
     await dispatch(fetchGraphCases());
-    if (data !== null) {
-      getAllData();
-    }
+    getAllData();
   }, []);
 
   //this function helps us update our state for the datasets on the chart.
@@ -44,7 +41,8 @@ const Chart = () => {
   //this function creates an array of data from our api response.
   const getChartValues = (data) => {
     let chartValues = [];
-    data !== null &&
+    data &&
+      Object.keys(data).length > 0 &&
       Object.keys(data).map((option) => {
         chartValues.push(data[option]);
       });
@@ -53,76 +51,74 @@ const Chart = () => {
 
   //this function renders our chart data
   const renderChart = () => {
-    if (data !== null && !loading) {
-      const data = {
-        labels: graphDates,
-        datasets: [
-          {
-            label: "Cases",
-            lineTension: 0.1,
-            borderColor: "#00FF07",
-            backgroundColor: "#c3ecc3",
-            data: graphCases,
-            pointStyle: "circle",
-          },
+    const data = {
+      labels: graphDates,
+      datasets: [
+        {
+          label: "Cases",
+          lineTension: 0.1,
+          borderColor: "#00FF07",
+          backgroundColor: "#c3ecc3",
+          data: graphCases,
+          pointStyle: "circle",
+        },
 
-          {
-            label: "Recovered",
-            lineTension: 0.1,
-            data: recoveryCases,
-            pointStyle: "circle",
-            backgroundColor: "#b980e2",
-            borderColor: "#6a0dad",
+        {
+          label: "Recovered",
+          lineTension: 0.1,
+          data: recoveryCases,
+          pointStyle: "circle",
+          backgroundColor: "#b980e2",
+          borderColor: "#6a0dad",
+        },
+        {
+          label: "Deaths",
+          lineTension: 0.1,
+          borderColor: "#FF0005",
+          data: deathCases,
+          pointStyle: "circle",
+          backgroundColor: "rgb(255, 99, 0)",
+        },
+      ],
+    };
+    const options = {
+      pointStyle: "circle",
+      legend: { display: true },
+      tooltips: {
+        usePointStyle: true,
+        callbacks: {
+          title: function (labels) {
+            return "";
           },
+          label: (labels) => {
+            return currency(labels.value);
+          },
+        },
+      },
+      scales: {
+        xAxes: [
           {
-            label: "Deaths",
-            lineTension: 0.1,
-            borderColor: "#FF0005",
-            data: deathCases,
-            pointStyle: "circle",
-            backgroundColor: "rgb(255, 99, 0)",
+            gridLines: { display: false },
           },
         ],
-      };
-      const options = {
-        pointStyle: "circle",
-        legend: { display: true },
-        tooltips: {
-          usePointStyle: true,
-          callbacks: {
-            title: function (labels) {
-              return "";
-            },
-            label: (labels) => {
-              return currency(labels.value);
-            },
-          },
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: { display: false },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: { display: true },
-              ticks: {
-                beginAtZero: true,
+        yAxes: [
+          {
+            gridLines: { display: true },
+            ticks: {
+              beginAtZero: true,
 
-                callback: (label) => {
-                  return currency(label);
-                },
+              callback: (label) => {
+                return currency(label);
               },
             },
-          ],
-        },
-      };
-      return <Line data={data} options={options} />;
-    }
+          },
+        ],
+      },
+    };
+    return <Line data={data} options={options} />;
   };
 
-  if (data !== null && !loading) {
+  if (data) {
     return (
       <div className="chart-container">
         <div className="line-chart-graph">
