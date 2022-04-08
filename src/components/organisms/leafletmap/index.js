f8525718c1d2b676868ace0c5c4e1f1fc7521584
soffyjay <aminusofiyat@gmail.com>
2022-04-08 21:31:22 +0100

    fix error

--- src/components/organisms/leafletmap/index.js
+++ src/components/organisms/leafletmap/index.js
@@ -17,45 +17,47 @@ const Map = () => {
     loading: state.countryReducer.loading,
     error: state.countryReducer.error,
   }));
+  if (data) {
+    return (
+      <div>
+        <h2 className="map-title">
+          Map Showing Each Countries and No of Recorded Cases
+        </h2>
 
-  return (
-    <div>
-      <h2 className="map-title">
-        Map Showing Each Countries and No of Recorded Cases
-      </h2>
-      <MapContainer center={[51.505, -0.09]} zoom={6}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {data &&
-          data?.map((country) => (
-            <Marker
-              position={[country.countryInfo.lat, country.countryInfo.long]}
-              key={country.country}
-            >
-              <Popup maxWidth={200} maxHeight={"auto"}>
-                <div>
-                  <span className="country-image">
-                    <img
-                      className="flag"
-                      src={country.countryInfo.flag}
-                      alt={country.name}
-                    />
-                  </span>
-                  <div className="country-info">
-                    <p>Name of Country : {country.country}</p>
-                    <p>No of Active Cases :{currency(country.active)}</p>
-                    <p> No of Recovered :{currency(country.recovered)}</p>
-                    <p> No of Deaths : {currency(country.deaths)}</p>
+        <MapContainer center={[51.505, -0.09]} zoom={6}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {data &&
+            data?.map((country) => (
+              <Marker
+                position={[country.countryInfo.lat, country.countryInfo.long]}
+                key={country.country}
+              >
+                <Popup maxWidth={200} maxHeight={"auto"}>
+                  <div>
+                    <span className="country-image">
+                      <img
+                        className="flag"
+                        src={country.countryInfo.flag}
+                        alt={country.name}
+                      />
+                    </span>
+                    <div className="country-info">
+                      <p>Name of Country : {country.country}</p>
+                      <p>No of Active Cases :{currency(country.active)}</p>
+                      <p> No of Recovered :{currency(country.recovered)}</p>
+                      <p> No of Deaths : {currency(country.deaths)}</p>
+                    </div>
                   </div>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-      </MapContainer>
-    </div>
-  );
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+      </div>
+    );
+  }
 };
 
 export default Map;
